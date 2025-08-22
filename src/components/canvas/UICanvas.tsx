740105c163a415ff9bfff5e54443590ff5944caa
DavidBerryUK <DavidBerryUK@hotmail.co.uk>
2025-08-22 18:57:54 +0100

    fine tune events.

--- src/components/canvas/UICanvas.tsx
+++ src/components/canvas/UICanvas.tsx
@@ -1,11 +1,11 @@
-import React, { useRef, useEffect } from 'react';
-import useEventHandlersMouse from './hooks/UseEventHandlersMouse';
-import useDrawCanvas from './hooks/UseDrawCanvas';
-import useEventHandlersKeyboard from './hooks/UseEventHandlersKeyboard';
-import useHandleTouchEvents from './hooks/UseHandleTouchEvents';
-import useHandleCanvasResize from './hooks/UseHandleCanvasResize';
 import CanvanContext from './models/canvasContext';
 import DemoSceneProvider from '../../sceneProviders/demo/DemoSceneProvider';
+import React, { useRef, useEffect } from 'react';
+import useDrawCanvas from './hooks/UseDrawCanvas';
+import useEventHandlersKeyboard from './hooks/UseEventHandlersKeyboard';
+import useEventHandlersMouse from './hooks/UseEventHandlersMouse';
+import useHandleCanvasResize from './hooks/UseHandleCanvasResize';
+import useHandleTouchEvents from './hooks/UseHandleTouchEvents';
 
 // Predefined zoom regions
 const regions: Record<string, { x: number; y: number; width: number; height: number }> = {
@@ -19,21 +19,24 @@ const CanvasViewer: React.FC = () => {
 	const canvasRef = useRef<HTMLCanvasElement | null>(null);
 	const context = new CanvanContext();
 	const sceneProvider = new DemoSceneProvider();
-
 	const { render } = useDrawCanvas(context, canvasRef, sceneProvider, regions);
 	const { handleMouseDownEvent, handleMouseMouseEvent, handleMouseUpEvent, handleWheelEvent } = useEventHandlersMouse(context, canvasRef);
 	const { handleKeyDownEvent } = useEventHandlersKeyboard(context, canvasRef, regions);
 	const { handleTouchStartEvent, handleTouchMoveEvent } = useHandleTouchEvents(context, canvasRef);
 	useHandleCanvasResize(canvasRef);
 
+	const cleanUp = () => {
+		const canvas = canvasRef.current!;
+	};
+
 	useEffect(() => {
 		const canvas = canvasRef.current!;
 
 		// Mouse + touch handlers
 		canvas.addEventListener('mousedown', handleMouseDownEvent);
-		canvas.addEventListener('mousemove', handleMouseMouseEvent);
+		window.addEventListener('mousemove', handleMouseMouseEvent);
 		window.addEventListener('mouseup', handleMouseUpEvent);
-		canvas.addEventListener('wheel', handleWheelEvent, { passive: false });
+		window.addEventListener('wheel', handleWheelEvent, { passive: false });
 		// Pinch zoom
 		canvas.addEventListener('touchstart', handleTouchStartEvent, { passive: false });
 		canvas.addEventListener('touchmove', handleTouchMoveEvent, { passive: false });
@@ -42,6 +45,18 @@ const CanvasViewer: React.FC = () => {
 
 		// render canvas
 		render();
+
+		// Cleanup function to remove listeners
+		return () => {
+			canvas.removeEventListener('mousedown', handleMouseDownEvent);
+			window.removeEventListener('mousemove', handleMouseMouseEvent);
+			window.removeEventListener('mouseup', handleMouseUpEvent);
+			window.removeEventListener('wheel', handleWheelEvent);
+			canvas.removeEventListener('touchmove', handleTouchMoveEvent);
+			canvas.removeEventListener('touchstart', handleTouchStartEvent);
+
+			window.removeEventListener('keydown', handleKeyDownEvent);
+		};
 	}, []);
 
 	return (
